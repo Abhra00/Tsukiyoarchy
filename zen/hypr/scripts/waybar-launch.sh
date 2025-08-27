d3eef15992b177df23f0c3273a01f19774ee180c
Abhra00 <abhramondal9674@gmail.com>
2025-08-28 00:46:41 +0530

    Zen folder cleaned up ✨

--- zen/hypr/scripts/waybar-launch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#  ┓ ┏┏┓┓┏┳┓┏┓┳┓  ┓ ┏┓┳┳┳┓┏┓┓┏
-#  ┃┃┃┣┫┗┫┣┫┣┫┣┫━━┃ ┣┫┃┃┃┃┃ ┣┫
-#  ┗┻┛┛┗┗┛┻┛┛┗┛┗  ┗┛┛┗┗┛┛┗┗┛┛┗
-#
-
-source "$HOME"/.config/waybar/colors.env
-envsubst <~/.config/waybar/config.jsonc | setsid uwsm app -- waybar -c /dev/stdin &
