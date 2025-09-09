95854de495c226915b1feca831b82e848dc1365b
Abhra00 <abhramondal9674@gmail.com>
2025-09-10 01:31:46 +0530

    Added new rofi theme ✨

--- hypr/scripts/launcher.sh
+++ hypr/scripts/launcher.sh
@@ -5,7 +5,7 @@
 #
 
 # Style-theme
-style_theme="$HOME/.config/rofi/launcher1.rasi"
+style_theme="$HOME/.config/rofi/launcher6.rasi"
 
 # Run
 pkill rofi || true && rofi -show drun -theme "${style_theme}" -run-command "uwsm app -- {cmd}"
